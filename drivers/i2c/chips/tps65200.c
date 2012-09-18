6f25451c2b2e52b8669cdbcc8e433b3b623f5598
DevChun <baljern0603@gmail.com>
2012-09-18 21:55:07 +0900

    Fixed build for spade by chrisch1974

--- drivers/i2c/chips/tps65200.c
+++ drivers/i2c/chips/tps65200.c
@@ -703,6 +703,9 @@ static void tps65200_int_func(struct work_struct *work)
 			tps65200_set_chg_stat(0);
 			tps65200_i2c_write_byte(0x29, 0x01);
 			tps65200_i2c_write_byte(0x28, 0x00);
+#if (defined(CONFIG_BATTERY_DS2746) && (!defined(CONFIG_MACH_SPADE)))
+			reverse_protection_handler(REVERSE_PROTECTION_HAPPEND);
+#endif
 			send_tps_chg_int_notify(CHECK_INT2, 1);
 			cancel_delayed_work(&chg_int_data->int_work);
 			enable_irq(chg_int_data->gpio_chg_int);
