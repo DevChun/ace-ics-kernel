98ad6f3bfbbe666130923851fd2b970395a99b2c
DevChun <baljern0603@gmail.com>
2012-09-28 22:56:39 +0900

    [FIX] Linaro CIFS Fix

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 3 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 PREEMPT Fri Sep 28 22:08:03 KST 2012"
+#define UTS_VERSION "#3 PREEMPT Fri Sep 28 22:27:21 KST 2012"
 #define LINUX_COMPILE_BY "chun"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.7.2 20120731 (prerelease) (Linaro GCC 4.7-2012.08) "
